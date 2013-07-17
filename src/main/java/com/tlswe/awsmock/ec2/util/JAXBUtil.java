d0a46f09b99c8b37f85a97251c109b8ca1c34162
maxiaohao <maxiaohao@gmail.com>
2013-07-18 00:14:52 +0800

    Add documentation to JAXBUtil.java

--- src/main/java/com/tlswe/awsmock/ec2/util/JAXBUtil.java
+++ src/main/java/com/tlswe/awsmock/ec2/util/JAXBUtil.java
@@ -1,9 +1,6 @@
 package com.tlswe.awsmock.ec2.util;
 
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -12,23 +9,34 @@ import javax.xml.bind.Marshaller;
 import javax.xml.namespace.QName;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 import com.tlswe.awsmock.common.util.PropertiesUtils;
 
+/**
+ * Utility class to build XML string as AWS response from java object, using the
+ * JAXB API, working with the stub classes under
+ * com.tlswe.awsmock.ec2.cxf_generated those describe the EC2 web service.
+ * 
+ * @author xma
+ * 
+ */
 public class JAXBUtil {
 
-    static private Log _log = LogFactory.getLog(JAXBUtil.class);
-
+    /**
+     * the JAXB context working in the context path of package
+     * com.tlswe.awsmock.ec2.cxf_generated
+     */
     static JAXBContext jaxbContext = null;
+
+    /**
+     * marshaller for building xml
+     */
     static Marshaller jaxbMarshaller = null;
 
     static {
         try {
 
-            jaxbContext = JAXBContext
-                    .newInstance("com.tlswe.awsmock.ec2.cxf_generated");
+            jaxbContext = JAXBContext.newInstance("com.tlswe.awsmock.ec2.cxf_generated");
             jaxbMarshaller = jaxbContext.createMarshaller();
 
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
@@ -42,93 +50,34 @@ public class JAXBUtil {
     }
 
     /**
-     * @param args
+     * 
+     * @param obj
+     *            object to be serialized and built into xml
+     * @param localPartQName
+     *            local part of the QName
+     * @param requestVersion
+     *            the version of EC2 API used by client (aws-sdk, cmd-line tools
+     *            or other third-party client tools)
+     * @return xml representation bound to the given object
      * @throws JAXBException
-     * @throws NoSuchAlgorithmException
-     * @throws UnsupportedEncodingException
      */
-    public static void main(String[] args) throws JAXBException,
-            NoSuchAlgorithmException, UnsupportedEncodingException {
-
-        // DescribeInstancesResponseType d = new
-        // DescribeInstancesResponseType();
-        // d.setRequestId(UUID.randomUUID().toString());
-        // ReservationSetType resSet = new ReservationSetType();
-        // ReservationInfoType resInfo1 = new ReservationInfoType();
-        // resInfo1.setOwnerId("aabb");
-        // resSet.getItem().add(resInfo1);
-        //
-        // RunningInstancesSetType instsSet = new RunningInstancesSetType();
-        //
-        // RunningInstancesItemType inst = new RunningInstancesItemType();
-        // inst.setInstanceId("i-mock001");
-        //
-        // InstanceStateType st = new InstanceStateType();
-        // st.setName("stopped-my");
-        //
-        // inst.setInstanceState(st);
-        // instsSet.getItem().add(inst);
-        //
-        // resInfo1.setInstancesSet(instsSet);
-        //
-        // d.setReservationSet(resSet);
-        //
-        // marshall4(d);
-
-
-    }
-
-
-
-    // private static void marshall2(Object obj) throws JAXBException {
-    //
-    // JAXBContext jaxbContext = JAXBContext
-    // .newInstance("com.tlswe.propellerlabs.awsmock.cxf_generated");
-    // Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-    // // output pretty printed
-    // jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-    // jaxbMarshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
-    //
-    // // jaxbMarshaller.marshal(new JAXBElement(new QName(obj.getClass()
-    // // .getSimpleName()), obj.getClass(), obj), System.out);
-    //
-    // jaxbMarshaller.marshal(new JAXBElement(new QName("aaa"),
-    // obj.getClass(), obj), System.out);
-    // }
-    //
-    // public static <T> void marshall(Class<T> clazz, T obj, Writer writer)
-    // throws JAXBException {
-    //
-    // jaxbMarshaller.marshal(
-    // new JAXBElement<T>(new QName(PropertiesUtils
-    // .getProperty("xmlns.current"), obj.getClass()
-    // .getSimpleName()), clazz, obj), writer);
-    //
-    // }
-
     @SuppressWarnings({ "unchecked", "rawtypes" })
-    public static String marshall(Object obj, String localPartQName,
-            String requestVersion) throws JAXBException {
+    public static String marshall(Object obj, String localPartQName, String requestVersion) throws JAXBException {
 
         StringWriter writer = new StringWriter();
 
-        jaxbMarshaller.marshal(
-                new JAXBElement(new QName(PropertiesUtils
-                        .getProperty("xmlns.current"), localPartQName), obj
-                        .getClass(), obj), writer);
+        jaxbMarshaller.marshal(new JAXBElement(new QName(PropertiesUtils.getProperty("xmlns.current"), localPartQName),
+                obj.getClass(), obj), writer);
 
         String ret = writer.toString();
 
-        if ("true".equalsIgnoreCase(PropertiesUtils
-                .getProperty("elasticfox.compatible"))
-                && null != requestVersion
-                && requestVersion.equals(PropertiesUtils
-                        .getProperty("ec2.api.version.elasticfox"))) {
-            ret = StringUtils
-                    .replaceOnce(ret, PropertiesUtils
-                            .getProperty("ec2.api.version.current.impl"),
-                            PropertiesUtils
-                                    .getProperty("ec2.api.version.elasticfox"));
+        /*- If elasticfox.compatible set to true, we replace the version number in the xml
+         * to match the version of elasticfox so that it could successfully accept the xml as reponse.
+         */
+        if ("true".equalsIgnoreCase(PropertiesUtils.getProperty("elasticfox.compatible")) && null != requestVersion
+                && requestVersion.equals(PropertiesUtils.getProperty("ec2.api.version.elasticfox"))) {
+            ret = StringUtils.replaceOnce(ret, PropertiesUtils.getProperty("ec2.api.version.current.impl"),
+                    PropertiesUtils.getProperty("ec2.api.version.elasticfox"));
         }
 
         // _log.info(ret);
@@ -136,13 +85,5 @@ public class JAXBUtil {
         return ret;
 
     }
-
-    // public static void marshall3(Object obj, Writer writer)
-    // throws JAXBException {
-    //
-    // jaxbMarshaller.marshal(new JAXBElement(new QName(obj.getClass()
-    // .getSimpleName()), obj.getClass(), obj), writer);
-    //
-    // }
 
 }
