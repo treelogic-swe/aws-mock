bcf3160ee34d36a7165396612d4ad5d84509ff3b
ylu <2295857079@qq.com>
2013-08-09 13:22:41 -0400

    Delete pack.bat
    Clean up this file and run wsdl2java in gradle task way.

--- pack.bat
+++
@@ -1,1 +0,0 @@
-Enter file contents herewsdl2java -verbose -d src/main/java -p com.tlswe.awsmock.ec2.cxf_generated -autoNameResolution -impl -server -frontend jaxws21 src/third_party/ec2-2013-02-01.wsdl
