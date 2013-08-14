d88e44574db108bb072b17dba535ad46f6df91ac
maxiaohao <maxiaohao@gmail.com>
2013-08-15 01:26:02 +0800

    Fixed template loading (find in directory -> find by classloader, in order that possible to package temlates into jar); Removed self-made cache for template loading as FreeMarker does that internally.

--- src/main/java/com/tlswe/awsmock/common/util/TemplateUtils.java
+++ src/main/java/com/tlswe/awsmock/common/util/TemplateUtils.java
@@ -1,10 +1,8 @@
 package com.tlswe.awsmock.common.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -34,21 +32,11 @@ public class TemplateUtils {
      */
     private static Configuration _conf = new Configuration();
 
+    // tell FreeMarker where to load templates - from folder "templates", in
+    // classpath
     static {
-        try {
-            // tell FreeMarker where to get templates
-            _conf.setDirectoryForTemplateLoading(new File(Thread.currentThread().getContextClassLoader()
-                    .getResource("templates").getFile()));
-        } catch (IOException e) {
-            _log.fatal("error occured while setting template folder from which we read all template files: ");
-        }
+        _conf.setClassForTemplateLoading(TemplateUtils.class, "/templates");
     }
-
-    /**
-     * a map of loaded Templates for re-usage (avoid loading template
-     * repeatedly)
-     */
-    private static Map<String, Template> _loadedTemplates = new HashMap<String, Template>();
 
     /**
      * Generate result from given template and data and print to writer.
@@ -64,17 +52,17 @@ public class TemplateUtils {
     public static void write(final String templateFilename, final Map<String, Object> data, final Writer writer)
             throws AwsMockException {
 
-        Template tmpl = _loadedTemplates.get(templateFilename);
-        if (null == tmpl) {
-            try {
-                tmpl = _conf.getTemplate(templateFilename);
-            } catch (IOException e) {
-                String errMsg = "IOException: failed to getTemplate (filename is " + templateFilename + ")";
-                _log.fatal(errMsg + ": " + e.getMessage());
-                throw new AwsMockException(errMsg, e);
-            }
-            _loadedTemplates.put(templateFilename, tmpl);
+        Template tmpl = null;
+        try {
+            // note that we don't need to cache templates by ourselves since
+            // getTemplate() does that internally already
+            tmpl = _conf.getTemplate(templateFilename);
+        } catch (IOException e) {
+            String errMsg = "IOException: failed to getTemplate (filename is " + templateFilename + ")";
+            _log.fatal(errMsg + ": " + e.getMessage());
+            throw new AwsMockException(errMsg, e);
         }
+
         try {
             tmpl.process(data, writer);
         } catch (TemplateException e) {
