7b53c85522f2f11b29023ceee4e69ea746c333f1
maxiaohao <maxiaohao@gmail.com>
2016-09-29 11:24:41 +0800

    Use DefaultMockEc2Instance instead of CustomMockEc2Instance, which is in 'example' source tree and should be secluded from 'src'; Replace deprecated assertion class 'junit.framework.Assert' with 'org.junit.Assert'

--- example/java/test/TestExamples.java
+++ example/java/test/TestExamples.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
